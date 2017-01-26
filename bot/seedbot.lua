734415b8bb2257040795ed9771bc07205b481a7b
ehsanmoeenasa <smartphonetelegram@gmail.com>
2017-01-26 05:02:06 -0800

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -232,7 +232,8 @@ function create_config( )
 	"whitelist",
 	"msg_checks"
     },
-    sudo_users = {110626080,103649648,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {110626080,103649648,111020322,0,tonumber(287315253
+)},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v4
 An advanced administration bot based on TG-CLI written in Lua
